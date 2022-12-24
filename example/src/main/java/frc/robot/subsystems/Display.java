c2896e6d1ab25eaa849ed68bea69a81c22f68e52
SilicaSandwhich <silicasandwhich@gmail.com>
2022-12-24 02:25:42 -0500

    added battery control & global debug control

--- example/src/main/java/frc/robot/subsystems/Display.java
+++ example/src/main/java/frc/robot/subsystems/Display.java
@@ -2,12 +2,12 @@ package frc.robot.subsystems;
 
 import java.text.DecimalFormat;
 
+import org.usfirst.frc.team3487.REVDigitMXPDisplay;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.usfirst.frc.team3487.REVDigitMXPDisplay;
 
 public class Display extends SubsystemBase{
 
@@ -21,7 +21,8 @@ public class Display extends SubsystemBase{
         displayBoard = REVDigitMXPDisplay.getInstance();
         stringEntry = Shuffleboard.getTab("Display").add("String","3487").getEntry();
         state = "idle";
-        df_obj = new DecimalFormat("##.0");
+        df_obj = new DecimalFormat("##.0V");
+        displayBoard.setDebug(true);
         displayBoard.clear();
         idleTimer = new Timer();
         idleTimer.start();
@@ -32,13 +33,8 @@ public class Display extends SubsystemBase{
         infoLoop();
     }
 
-    public void battery(Boolean debug) {
-        double voltage = RobotController.getBatteryVoltage();
-        displayBoard.displayText(df_obj.format(voltage).concat("V"),debug);
-        
-    }
     public void battery() {
-        battery(false);
+        displayBoard.displayBattery();
         
     }
     
@@ -51,7 +47,7 @@ public class Display extends SubsystemBase{
             if(displayBoard.getBButtonPressed()){
                 state="custom";
             }
-            idle(true);
+            idle();
         }else if(state=="battery"){
             if(displayBoard.getAButtonPressed()){
                 state="idle";
@@ -59,7 +55,7 @@ public class Display extends SubsystemBase{
             if(displayBoard.getBButtonPressed()){
                 state="custom";
             }
-            battery(true);
+            battery();
         }else if(state=="custom"){
             if(displayBoard.getAButtonPressed()){
                 state="battery";
@@ -67,32 +63,18 @@ public class Display extends SubsystemBase{
             if(displayBoard.getBButtonPressed()){
                 state="idle";
             }
-            customDisplay(true);
+            customDisplay();
         }
     }
 
     @Override
     public void simulationPeriodic(){
-    }   
-
-    public boolean getAButtonPressed(){
-        return displayBoard.getAButtonPressed();
     }
-
-    public boolean getBButtonPressed(){
-        return displayBoard.getBButtonPressed();
-    }
-    public void idle(boolean debug){
-        displayBoard.displayScrollText("3487", 0.2, debug);
-    }
-
     public void idle(){
-        idle(false);
+        displayBoard.displayScrollText("3487", 0.2);
     }
-    public void customDisplay(Boolean debug){
-        displayBoard.displayScrollText(stringEntry.getString("3487"), 0.2, true);
-    }
+
     public void customDisplay(){
-        customDisplay(false);
+        displayBoard.displayScrollText(stringEntry.getString("3487"), 0.2);
     }
 }
